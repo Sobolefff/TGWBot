c28dfb19af4c983eddb5934a19e7624b32d310d4
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2025-07-11 10:12:01 +0300

    Create HealthServer.kt

---
+++ src/main/kotlin/bot/utils/HealthServer.kt
@@ -0,0 +1,17 @@
+package org.example.health
+
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+
+fun startHealthServer() {
+    val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
+    embeddedServer(Netty, port = port) {
+        routing {
+            get("/health") {
+                call.respondText("OK")
+            }
+        }
+    }.start(wait = false) // НЕ блокирует основной поток (бот продолжает работать)
+}
