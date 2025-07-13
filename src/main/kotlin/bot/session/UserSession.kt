58cd0308f358c578c18203b60519ef9b1f42231f
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2025-07-13 11:40:17 +0300

    Update UserSession.kt

--- src/main/kotlin/bot/session/UserSession.kt
+++ src/main/kotlin/bot/session/UserSession.kt
@@ -2,5 +2,6 @@ package org.example.bot.session
 
 data class UserSession (
     var country: String = "",
+    var confirmationMsgId: Long? = null,
     val tempMessageIds: MutableList<Int?> = mutableListOf()
 )
